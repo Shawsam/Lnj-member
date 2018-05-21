9d168b4d4a4352405ff8245f9df3eb26849db0f1
shawsam <1603334317@qq.com>
2018-05-21 12:42:12 +0800

    官方用户信息授权接口现在只能主动授权

--- app.js
+++ app.js
@@ -23,106 +23,27 @@ App({
       if (this.globalData.userInfo) {           
         typeof cb == "function" && cb(this.globalData.userInfo)
       } else {
-        //调用微信登录接口，获取code
-        wx.login({
-            success: function (r) {
-                var code = r.code;        //登录凭证
-                if (code) {
-                    //调用获取用户信息接口
-                    wx.getUserInfo({
-                        withCredentials: true,
-                        success: function (res) {
-                            //基本信息
-                            console.log(res);
-                            _this.globalData.userInfo = res.userInfo;
-                            typeof cb == "function" && cb(_this.globalData.userInfo);
-
-                            //请求服务器，解密用户信息 获取unionId等加密信息
-                            var param = { mini:'mini',
-                                          jsCode:code
-                                        };
-                            wx.request({
-                                url: _this.globalData.host+'/getMiniOpen', 
-                                method:'POST',
-                                header: {  "Content-Type": "application/x-www-form-urlencoded" }, 
-                                data: param,
-                                success: function (res) {
-                                    //服务器返回的结果
-                                    console.log('信息');
-                                    console.log(res);
-                                    if (res.data.errcode == 0) {
-                                       var openId = res.data.miniOpenId,
-                                           _openId = res.data.openId,
-                                           userId = res.data.userId,
-                                           unionId = res.data.unionId,
-                                           mobile = res.data.mobile,
-                                           cardNo =  res.data.cardNo;
-                                       _this.globalData.openId = openId;
-                                       _this.globalData._openId = _openId;
-                                       _this.globalData.userId = userId;
-                                       _this.globalData.unionId = unionId;
-                                       _this.globalData.mobile = mobile;
-                                       _this.globalData.cardNo = cardNo;
-
-                                       // if (getCurrentPages().length != 0) {
-                                       //    getCurrentPages()[getCurrentPages().length - 1].onLoad()
-                                       // }
-                                       
-                                    } else {
-                                       console.log('服务器异常');
-                                       wx.redirectTo({ url:'../view_state/index?error='+res.statusCode+'&errorMsg=服务器异常'})
-                                    }
-
-                                },
-                                fail: function () {
-                                    console.log('网络错误，请重试')
-                                    wx.redirectTo({ url:'../view_state/index?errorMsg=网络错误，请重试'})
-                                }
-                            })
-                        },
-                        fail: function () {
-                            console.log('用户未授权,获取用户信息失败');
-                            wx.showModal({
-                                title: '用户未授权',
-                                content: '如需正常使用，请按确定并在授权管理中选中“用户信息”，然后点按确定。最后再重新进入小程序即可正常使用。',
-                                showCancel: false,
-                                success: function (res) {
-                                    if (res.confirm) {
-                                        //进入二次授权页面
-                                        wx.openSetting({
-                                            success: function(res) {
-                                                var user_Athority = res.authSetting['scope.userInfo'];
-                                                getCurrentPages()[getCurrentPages().length - 1].onLoad()
-                                            }
-                                        });
-                                    }
-                                }
-                            })
-                        }
-                    })
-
-                } else {
-                    console.log('获取用户登录态失败' + r.errMsg)
-                    var errMsg = '获取用户登录态失败' + r.errMsg;
-                    wx.redirectTo({ url:'../view_state/index?errorMsg='+errMsg })
+        // 查看是否授权
+        wx.getSetting({
+          success: function(res){
+            if (res.authSetting['scope.userInfo']) {
+              // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+              wx.getUserInfo({
+                success: function(res) {
+                  console.log(res.userInfo)
                 }
-            },
-            fail: function () {
-                 console.log('登陆失败')
-                 var errMsg = '登录失败';
-                 wx.redirectTo({ url:'../view_state/index?errorMsg='+errMsg })
+              })
+            }else{
+              wx.redirectTo({url:"../authorize/index"})
             }
+          }
         })
       }
   },
   onShow:function(scene){
     if(scene.path == "pages/takeOut_addr_add/index"){
         this.getUserInfo();
-    };
-    // if(getCurrentPages()[0] && getCurrentPages()[0].route != 'pages/entrace/index'){
-    //     wx.reLaunch({url:'../../pages/entrace/index'});
-    //     this.getUserInfo();
-    // }
+    }
   },
   onHide:function(){
     this.globalData.userInfo = null;            //会员状态的改变，用户进公众号再进入小程序
---
+++ pages/authorize/index.js
@@ -0,0 +1,77 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+     userInfo:null,
+     canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad:function(){
+  	    //调用微信登录接口，获取code
+        wx.login({
+            success: function (r) {
+                var code = r.code;        //登录凭证
+                if (code) {
+                    //请求服务器，解密用户信息 获取unionId等加密信息
+                    var param = { mini:'mini',
+                                  jsCode:code
+                                };
+                    wx.request({
+                        url: app.globalData.host+'/getMiniOpen', 
+                        method:'POST',
+                        header: {  "Content-Type": "application/x-www-form-urlencoded" }, 
+                        data: param,
+                        success: function (res) {
+                            //服务器返回的结果
+                            console.log('信息');
+                            console.log(res);
+                            if (res.data.errcode == 0) {
+                               var openId = res.data.miniOpenId,
+                                   _openId = res.data.openId,
+                                   userId = res.data.userId,
+                                   unionId = res.data.unionId,
+                                   mobile = res.data.mobile,
+                                   cardNo =  res.data.cardNo;
+                               app.globalData.openId = openId;
+                               app.globalData._openId = _openId;
+                               app.globalData.userId = userId;
+                               app.globalData.unionId = unionId;
+                               app.globalData.mobile = mobile;
+                               app.globalData.cardNo = cardNo;
+
+                               // if (getCurrentPages().length != 0) {
+                               //    getCurrentPages()[getCurrentPages().length - 1].onLoad()
+                               // }
+                               
+                            } else {
+                               console.log('服务器异常');
+                               wx.redirectTo({ url:'../view_state/index?error='+res.statusCode+'&errorMsg=服务器异常'})
+                            }
+
+                        },
+                        fail: function () {
+                            console.log('网络错误，请重试')
+                            wx.redirectTo({ url:'../view_state/index?errorMsg=网络错误，请重试'})
+                        }
+                    })
+                } else {
+                    console.log('获取用户登录态失败' + r.errMsg)
+                    var errMsg = '获取用户登录态失败' + r.errMsg;
+                    wx.redirectTo({ url:'../view_state/index?errorMsg='+errMsg })
+                }
+            },
+            fail: function () {
+                 console.log('登陆失败')
+                 var errMsg = '登录失败';
+                 wx.redirectTo({ url:'../view_state/index?errorMsg='+errMsg })
+            }
+        })
+  },
+  authorizeFun:function(res){
+  	 console.log(res.detail.userInfo)
+     app.globalData.userInfo = res.detail.userInfo
+     this.setData({userInfo:res.detail.userInfo})
+     wx.redirectTo({url:'../entrace/index'})
+  }
+
+})
