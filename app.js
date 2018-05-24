bdb73002cb21d4f484b4236a8b9329a623c0bde9
shawsam <1603334317@qq.com>
2018-05-24 10:06:27 +0800

    getSetting判断是否已经授权

--- app.js
+++ app.js
@@ -34,6 +34,62 @@ App({
                   cb(res.userInfo)
                 }
               })
+
+              //调用微信登录接口，获取code
+              wx.login({
+                  success: function (r) {
+                      var code = r.code;        //登录凭证
+                      if (code) {
+                          //请求服务器，解密用户信息 获取unionId等加密信息
+                          var param = { mini:'mini',
+                                        jsCode:code
+                                      };
+                          wx.request({
+                              url: _this.globalData.host+'/getMiniOpen', 
+                              method:'POST',
+                              header: {  "Content-Type": "application/x-www-form-urlencoded" }, 
+                              data: param,
+                              success: function (res) {
+                                  //服务器返回的结果
+                                  console.log('信息');
+                                  console.log(res);
+                                  if (res.data.errcode == 0) {
+                                     var openId = res.data.miniOpenId,
+                                         _openId = res.data.openId,
+                                         userId = res.data.userId,
+                                         unionId = res.data.unionId,
+                                         mobile = res.data.mobile,
+                                         cardNo =  res.data.cardNo;
+                                     _this.globalData.openId = openId;
+                                     _this.globalData._openId = _openId;
+                                     _this.globalData.userId = userId;
+                                     _this.globalData.unionId = unionId;
+                                     _this.globalData.mobile = mobile;
+                                     _this.globalData.cardNo = cardNo;
+                                  } else {
+                                     console.log('服务器异常');
+                                     wx.redirectTo({ url:'../view_state/index?error='+res.statusCode+'&errorMsg=服务器异常'})
+                                  }
+
+                              },
+                              fail: function () {
+                                  console.log('网络错误，请重试')
+                                  wx.redirectTo({ url:'../view_state/index?errorMsg=网络错误，请重试'})
+                              }
+                          })
+                      } else {
+                          console.log('获取用户登录态失败' + r.errMsg)
+                          var errMsg = '获取用户登录态失败' + r.errMsg;
+                          wx.redirectTo({ url:'../view_state/index?errorMsg='+errMsg })
+                      }
+                  },
+                  fail: function () {
+                       console.log('登陆失败')
+                       var errMsg = '登录失败';
+                       wx.redirectTo({ url:'../view_state/index?errorMsg='+errMsg })
+                  }
+              })
+
             }else{
               wx.redirectTo({url:"../authorize/index"})
             }
