7f3ea809b73d55986879eab60947e2fc43aedbab
shawsam <1603334317@qq.com>
2019-06-17 17:03:22 +0800

    .gitignore

--- utils/config.js
+++
@@ -1,7 +0,0 @@
-//全局配置
-const config = {};
-config.mode = "run";                             // 运行模式
-config.host = "https://weixin.chinauff.com";     // 接口域名
-// config.mode = "dev";                          // 开发模式
-// config.host = "https://demo.micvs.com";       // 接口域名
-module.exports = config 
